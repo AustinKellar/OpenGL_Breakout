4930d13b96ec33236d5abcfedd43267630be3d62
AustinKellar <akellar@ucsc.edu>
2019-06-23 13:40:08 -0700

    Got rid of AreaOfTriangle function in the CollisionDetector because it is not needed any more

--- Breakout/CollisionDetector.cpp
+++ Breakout/CollisionDetector.cpp
@@ -9,11 +9,6 @@ CollisionDetector::CollisionDetector(std::vector<GameObject*>* gameObjects)
 	this->gameObjects = gameObjects;
 }
 
-float AreaOfTriangle(Vector2* p1, Vector2* p2, Vector2* p3)
-{
-	return ( ( p1->x * ( p2->y - p3->y ) ) + ( p2->x * ( p3->y - p1->y ) ) + ( p3->x * ( p1->y - p2->y ) ) / 2.f );
-}
-
 bool IsCollision(GameObject* obj1, GameObject* obj2)
 {
 	float w = obj1->halfWidth + obj2->halfWidth;
